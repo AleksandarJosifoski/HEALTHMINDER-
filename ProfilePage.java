928aef69c49284beedf4e115907cd8363f875b5a
ahmadaoun30 <98710478+ahmadaoun30@users.noreply.github.com>
2022-04-22 13:33:32 -0400

    Add files via upload

---
+++ ProfilePage.java
@@ -0,0 +1,60 @@
+package com.example.profilepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void showUsername(View view) {
+        displayToast(getString(R.string.username));
+    }
+    public void showPassword(View view) {
+        displayToast(getString(R.string.password));
+    }
+    public void showProfileImage(View view) {
+        displayToast(getString(R.string.ProfileImage));
+    }
+}
